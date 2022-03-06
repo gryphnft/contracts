005a157fa625f251f2a7e64e1499f7c0222b159a
Chris Blanquera <chris@incept.asia>
2022-03-07 02:19:19 +0800

    gryph vesting for testing

--- contracts/GryphToken.sol
+++ contracts/GryphToken.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
@@ -21,8 +20,7 @@ import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 //  https://www.gry.ph/
 //
 
-contract GryphToken is 
-  Context, 
+contract GryphToken is
   Pausable,
   AccessControlEnumerable, 
   ERC20Burnable, 
